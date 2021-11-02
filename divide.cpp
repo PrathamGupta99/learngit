9bdd473298d7d578eb3437ba24637a502d980e2b
Pratham Gupta <pratham9902gupta@gmail.com>
2021-11-02 06:39:04 +0530

    adding divide.cpp

---
+++ divide.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    int div = a/b;
+    cout<<a<<" "<<b<<" ";
+    cout<<div;
+    cout<<endl;
+    return 0;
+}
