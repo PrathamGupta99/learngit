692727c2f9f28aad27a1d71798ea85a51934adcc
Pratham Gupta <pratham9902gupta@gmail.com>
2021-11-02 06:17:29 +0530

    added multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    int mul = a*b;
+    cout<<a<<" "<<b<<" ";
+    cout<<mul;
+    cout<<endl;
+    return 0;
+}
